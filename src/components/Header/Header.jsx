21e55cd30f2b2df8685de0259aa12b3c4307f29e
utsavbhesaniya2005 <ukbhesaniya01@gmail.com>
2024-12-07 10:29:19 +0530

    File Upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -14,11 +15,19 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className='ms-auto'>
-                        <Nav className='me-5'>
-                            <Link to='/add' className='navigate'>Product Add</Link>
-                        </Nav>
                         <Nav>
-                            <Link to='/' className='navigate'>Product Dashboard</Link>
+                            <Link to='/' className='navigate'>
+                                <Button className='btn2' >
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" height="30" fill='#ffffff'><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
+                                </Button>
+                            </Link>
+                        </Nav>
+                        <Nav className='ms-5'>
+                            <Link to='/add' className='navigate'>
+                                <Button className='btn2'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" height="30" fill='#ffffff'><path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3zM504 312l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                                </Button>
+                            </Link>
                         </Nav>
                     </Nav>
                     </Navbar.Collapse>
--- src/components/ProductDashboard/ProductDashboard.jsx
+++ src/components/ProductDashboard/ProductDashboard.jsx
@@ -120,12 +120,13 @@ const ProductDashboard = () => {
                                                 />
                                             </div>
                                             <div className="btns flex text-center mt-4">
-                                                <Button variant='primary' className='me-5 ms-5' onClick={() => handleEdit(data.id)}>Edit</Button> ||  
-                                                {/* || <Button variant='danger' className='ms-5' onClick={() => handleDelete(data.id)}>Delete</Button> */}
-                                                <Button variant="danger" className='ms-5' onClick={() => { 
+                                                <Button variant='primary' className='me-5 ms-5 p-2' onClick={() => handleEdit(data.id)}>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="30" fill='#ffffff'><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-96c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 64z"/></svg>    
+                                                </Button> || 
+                                                <Button variant="danger" className='ms-5 p-2' onClick={() => { 
                                                 setProductIdToDelete(data.id);
                                                 setModalShow(true); }}>
-                                                    Delete
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="30" fill='#ffffff'><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z" /></svg>
                                                 </Button>
                                                 <MyVerticallyCenteredModal 
                                                     show={modalShow}
